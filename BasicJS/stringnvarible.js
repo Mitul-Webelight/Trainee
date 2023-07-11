e8a74bba81031b27cba3e40001e104637e447482
Mitul Kasundra <mitulk@webelight.co.in>
2023-07-11 16:06:37 +0530

    initial commit

---
+++ BasicJS/stringnvarible.js
@@ -0,0 +1,16 @@
+// let city = 'hmedabad'
+// let state = 'Gujarat'
+// let location = city + ', ' + state
+// console.log(location)
+
+let isGuestOneVegan = true;
+
+let isGuestTwoVegan = true;
+
+if (isGuestOneVegan && isGuestTwoVegan) {
+  console.log("Only offer up vegan food.");
+} else if (isGuestOneVegan || isGuestTwoVegan) {
+  console.log("Make sure to offer up some vegan food.");
+} else {
+  console.log("Offer up anything on the menu.");
+}
