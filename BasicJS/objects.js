0761e72868932d9df2014ba92dd81be94cdc1576
Mitul-Webelight <mitulk@webelight.co.in>
2023-07-12 18:52:07 +0530

    push to gitlab

--- BasicJS/objects.js
+++ BasicJS/objects.js
@@ -6,31 +6,85 @@
 // myBook.title = 'Animal Farm'
 // console.log(`${myBook.title} by ${myBook.author}`)
 
-let myBook = {
-  title: "1984",
-  author: "George Orwell",
-  pageCount: 326,
-};
-let otherBook = {
-  title: "A Peoples History",
-  author: "Howard Zinn",
-  pageCount: 723,
-};
-let getSummary = function (book) {
-  console.log(`${book.title} by ${book.author}`);
+// let myBook = {
+//   title: "1984",
+//   author: "George Orwell",
+//   pageCount: 326,
+// };
+// let otherBook = {
+//   title: "A Peoples History",
+//   author: "Howard Zinn",
+//   pageCount: 723,
+// };
+// let getSummary = function (book) {
+//   console.log(`${book.title} by ${book.author}`);
+// };
+
+// getSummary(otherBook);
+// // getSummary(myBook);
+
+// let convertFahrenheit = function (fahrenheit) {
+//   return {
+//     fahrenheit: fahrenheit,
+//     kelvin: (fahrenheit + 459.67) * (5 / 9),
+//     celsius: (fahrenheit - 32) * (5 / 9),
+//   };
+// };
+// let temps = convertFahrenheit(74);
+// console.log(temps);
+
+// let myAccount = {
+// name: 'Mitul',
+// expenses: 0,
+// income: 0
+// }
+// let addExpense = function (account, amount) {
+// account.expenses = account.expenses + amount
+// console.log(account)
+// }
+// addExpense(myAccount, 2000)
+// console.log(myAccount)
+
+// let myAccount = {
+// name: 'Mitul',
+// expenses: 0,
+// income: 0
+// }
+// let addExpense = function (account, amount) {
+// account = {}
+// account.age = 1
+// console.log(account)
+// }
+// addExpense(myAccount, 2000)
+// console.log(myAccount)
+
+let myAccount = {
+  name: "Mitul",
+  expenses: 0,
+  income: 0,
 };
 
-getSummary(otherBook);
-// getSummary(myBook);
-
-let convertFahrenheit = function (fahrenheit) {
-  return {
-    fahrenheit: fahrenheit,
-    kelvin: (fahrenheit + 459.67) * (5 / 9),
-    celsius: (fahrenheit - 32) * (5 / 9),
-  };
+let addExpense = function (account, amount) {
+  account.expenses = account.expenses + amount;
 };
-let temps = convertFahrenheit(74);
-console.log(temps);
-// https://github.com/Mitul-Webelight/Test.git 
-//glpat-XFf7BSwZUnn3CzBo7zVJ
+
+let addIncome = function (account, income) {
+  account.income = account.income + income;
+};
+
+let resetAccount = function (account) {
+  account.expenses = 0;
+  account.income = 0;
+};
+
+let getAccountSummary = function (account) {
+  let balance = account.income - account.expenses;
+  return `Account for ${account.name} has $${balance}. $${account.income} in income. $${account.expenses} in expenses.`;
+};
+
+addIncome(myAccount, 2000);
+addExpense(myAccount, 2.5);
+addExpense(myAccount, 160);
+console.log(getAccountSummary(myAccount));
+resetAccount(myAccount);
+console.log(getAccountSummary(myAccount));
