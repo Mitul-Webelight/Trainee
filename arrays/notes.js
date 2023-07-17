3eb644f7d6c947324111b77905e8a80b4bcff68f
Mitul-Webelight <mitulk@webelight.co.in>
2023-07-17 18:14:35 +0530

    end of the day

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,6 +1,19 @@
 // const names = ["A", "B", "C", "D"];
-
 // console.log(names[2]);
-
 // console.log(names[names.length - 1]);
+
+// const nums = [1];
+// nums.push(12);
+// nums.unshift(3);
+// console.log(nums);
+
+// const nums = [99, 199, 299]
+// nums.splice(1, 0, 399)
+// console.log(nums) 
+
+// const nums = [10, 20, 30, 40]
+// nums.pop()
+// nums.pop()
+// nums.shift()
+// console.log(nums)
 
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,6 +1,19 @@
-const todo = ["ME", "I", "YOU", "HE", "THEY"];
+// const todo = ["ME", "I", "YOU", "HE", "THEY"];
 
-console.log(`You have ${todo.length} todos!`);
+// console.log(`You have ${todo.length} todos!`);
 
-console.log(todo[0]);
-console.log(todo[todo.length - 1]);
+// console.log(todo[0]);
+// console.log(todo[todo.length - 1]);
+
+const todos = [
+  "Order cat food",
+  "Clean kitchen",
+  "Buy food",
+  "Do work",
+  "Exercise",
+];
+
+todos.forEach(function (todo, index) {
+  const num = index + 1;
+  console.log(`${num}. ${todo}`);
+});
