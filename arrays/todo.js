0615ab9f3f1dc8c8127b1a5c0c3110a59ef455ca
Mitul-Webelight <mitulk@webelight.co.in>
2023-07-18 15:46:23 +0530

    initial commit

--- arrays/todo.js
+++ arrays/todo.js
@@ -47,3 +47,31 @@
 
 // deleteTodo(todos, 'buy food')
 // console.log(todos)
+
+// const todos = [
+//   {
+//     text: "Buy food",
+//     completed: true,
+//   },
+//   {
+//     text: "Do work",
+//     completed: false,
+//   },
+//   {
+//     text: "Exercise",
+//     completed: true,
+//   },
+// ];
+// const sortTodos = function (todos) {
+//   todos.sort(function (a, b) {
+//     if (!a.completed && b.completed) {
+//       return -1;
+//     } else if (!b.completed && a.completed) {
+//       return 1;
+//     } else {
+//       return 0;
+//     }
+//   });
+// };
+// sortTodos(todos);
+// console.log(todos);
